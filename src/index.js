93a2735fa2b0e9067c4dc7cc4b2bccec821585d9
Isaac <isaachayab0@gmail.com>
2025-06-23 14:17:36 +0100

    First commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Middleware to log requests
+app.use((req, res, next) => {
+  console.log(`${req.method} request for '${req.url}'`);
+    next(); 
+});
+
+// Sample route
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
