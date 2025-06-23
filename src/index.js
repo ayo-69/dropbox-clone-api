2d842e6a7dd88e6e951ed7a848acafba65c4daa2
Isaac <isaachayab0@gmail.com>
2025-06-23 14:22:43 +0100

    -Fixed package.json -Editted logs for the logging middleware

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 // Middleware to log requests
 app.use((req, res, next) => {
-  console.log(`${req.method} request for '${req.url}'`);
+  console.log(`${req.method} -- '${req.url}'`);
     next(); 
 });
 
